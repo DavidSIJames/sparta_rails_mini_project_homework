3e30c29fcbfa69a3e0f55b28c6cf5743e17789bc
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-10-05 19:33:20 +0100

    added extra user fields. feeling less frustrated now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '879d6bb1063447253f459df465d4b55a3588b41907d06975828001940ec89e1b9052cbb68dd1a19502ca8ab1fc5fb9f146de4b4ba43d6a60f5feb83824b05007'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+   config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
