55c930cba799e9a736d166d3937fa6a670feaff4
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-10-05 19:52:35 +0100

    made comment a nested route of tech

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static#landing'
   devise_for :users
-  resources :technologies
+  resources :technologies do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
